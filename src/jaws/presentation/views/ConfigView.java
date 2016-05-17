4a3f81b6400d90e6796e860aec38f2f4dd8e6c90
overactor <rik.degraaff@gmx.ch>
2016-05-17 08:32:39 +0200

    added src folder to versioning (finally)

---
+++ src/jaws/presentation/views/ConfigView.java
@@ -0,0 +1,12 @@
+package jaws.presentation.views;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+
+public class ConfigView extends JFrame {
+
+	public ConfigView() {
+		
+		setLayout(new BoxLayout(parent, defaultCloseOperation));
+	}
+}

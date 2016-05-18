1242a7a2bf2975511366070137edafa9b99bbe22
overactor <rik.degraaff@gmx.ch>
2016-05-18 16:14:56 +0200

    started work on GUI

--- src/jaws/presentation/views/ConfigView.java
+++ src/jaws/presentation/views/ConfigView.java
@@ -1,12 +1,39 @@
 package jaws.presentation.views;
 
-import javax.swing.BoxLayout;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
 
 public class ConfigView extends JFrame {
 
+	JList logList;
+
 	public ConfigView() {
-		
-		setLayout(new BoxLayout(parent, defaultCloseOperation));
+
+		super("JAWS config client");
+
+		setLayout(new BorderLayout());
+
+		// config panel
+		JPanel configPanel = new JPanel(new GridLayout(2, 2));
+		add(configPanel, BorderLayout.NORTH);
+
+		// log panel
+		JPanel logPanel = new JPanel(new BorderLayout());
+
+		JPanel logPathPanel = new JPanel(new BorderLayout());
+		logPanel.add(logPathPanel, BorderLayout.NORTH);
+
+		logList = new JList<String>();
+		logPanel.add(logList, BorderLayout.CENTER);
+
+		add(logPanel, BorderLayout.CENTER);
+
+		// bottom panel
+		JPanel bottomPanel = new JPanel(new BorderLayout());
+		add(bottomPanel, BorderLayout.SOUTH);
 	}
 }

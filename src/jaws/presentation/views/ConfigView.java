4d990c2fd19878eca6845db30d92e505cc057188
overactor <rik.degraaff@gmx.ch>
2016-05-21 13:31:43 +0200

    added basic layout

--- src/jaws/presentation/views/ConfigView.java
+++ src/jaws/presentation/views/ConfigView.java
@@ -4,16 +4,32 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 public class ConfigView extends JFrame {
 
-	JList logList;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9041765805686943532L;
+	
+	JList<String> logList;
+	
+	JTextField logPathField;
+	
+	public static void main(String[] args) {
+		new ConfigView();
+	}
 
 	public ConfigView() {
 
 		super("JAWS config client");
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		setLayout(new BorderLayout());
 
@@ -26,14 +42,20 @@ public class ConfigView extends JFrame {
 
 		JPanel logPathPanel = new JPanel(new BorderLayout());
 		logPanel.add(logPathPanel, BorderLayout.NORTH);
+		logPathPanel.add(new JLabel("Logs"), BorderLayout.WEST);
+		logPathField = new JTextField();
+		logPathPanel.add(logPathField, BorderLayout.CENTER);
 
 		logList = new JList<String>();
-		logPanel.add(logList, BorderLayout.CENTER);
+		logPanel.add(new JScrollPane(logList), BorderLayout.CENTER);
 
 		add(logPanel, BorderLayout.CENTER);
 
 		// bottom panel
 		JPanel bottomPanel = new JPanel(new BorderLayout());
 		add(bottomPanel, BorderLayout.SOUTH);
+		
+		setBounds(200, 200, 400, 600);
+		setVisible(true);
 	}
 }

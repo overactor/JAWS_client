07b97a7d9ec915a93d38f2176a3ee3f22db88e96
overactor <rik.degraaff@gmx.ch>
2016-06-02 20:20:36 +0200

    GridBagLayout stuff

--- src/jaws/presentation/views/ConfigView.java
+++ src/jaws/presentation/views/ConfigView.java
@@ -1,10 +1,15 @@
 package jaws.presentation.views;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -18,6 +23,11 @@ public class ConfigView extends JFrame {
 	 *
 	 */
 	private static final long serialVersionUID = -9041765805686943532L;
+	
+	JComboBox<String> presetCombo;
+	JButton loadPresetButton, savePresetButton, newPresetButton;
+	
+	JTextField webrootField;
 
 	JList<String> logList;
 	JTextField logPathField;
@@ -32,8 +42,22 @@ public class ConfigView extends JFrame {
 		setLayout(new BorderLayout());
 
 		// config panel
-		JPanel configPanel = new JPanel(new GridLayout(2, 2));
+		JPanel configPanel = new JPanel(new GridBagLayout());
 		add(configPanel, BorderLayout.NORTH);
+		addWithConstraints(configPanel, new JLabel("Preset"), 0, 0);
+		presetCombo = new JComboBox<>();
+		addWithConstraints(configPanel, presetCombo, 1, 0, 2, 1);
+		
+		loadPresetButton = new JButton("load");
+		savePresetButton = new JButton("save");
+		newPresetButton = new JButton("new");
+		addWithConstraints(configPanel, loadPresetButton, 3, 0);
+		addWithConstraints(configPanel, savePresetButton, 4, 0);
+		addWithConstraints(configPanel, newPresetButton, 5, 0);
+		
+		addWithConstraints(configPanel, new JLabel("Webroot"), 0, 1);
+		webrootField = new JTextField();
+		addWithConstraints(configPanel, webrootField, 1, 1, 5, 1);
 
 		// log panel
 		JPanel logPanel = new JPanel(new BorderLayout());
@@ -66,5 +90,35 @@ public class ConfigView extends JFrame {
 
 		setBounds(200, 200, 400, 600);
 		setVisible(true);
+	}
+	
+	private static void addWithConstraints(Container container, JComponent component,
+	                               int gridx, int gridy) {
+		addWithConstraints(container, component, gridx, gridy, 1, 1);
+	}
+	
+	private static void addWithConstraints(Container container, JComponent component,
+	                               int gridx, int gridy,
+	                               int gridWidth, int gridHeight) {
+		addWithConstraints(container, component, gridx, gridy, gridWidth, gridHeight, gridWidth, gridHeight);
+	}
+		
+	private static void addWithConstraints(Container container, JComponent component,
+	                               int gridx, int gridy,
+	                               int gridWidth, int gridHeight,
+	                               int weightx, int weighty) {
+		
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.insets = new Insets(2, 5, 2, 5);
+		constraints.gridx = gridx;
+		constraints.gridy = gridy;
+		constraints.gridwidth = gridWidth;
+		constraints.gridheight = gridHeight;
+		constraints.weightx = weightx;
+		constraints.weighty = weighty;
+		
+		container.add(component);
+		((GridBagLayout) container.getLayout()).setConstraints(component, constraints);
 	}
 }

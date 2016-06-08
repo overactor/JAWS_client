a0cb509ba6c48938def20a008e90364326dbe6e2
vmuser <vmuser@192.168.61.128>
2016-06-08 14:46:23 +0200

    added PresetDelegate (for real)

---
+++ src/jaws/presentation/views/PresetDelegate.java
@@ -0,0 +1,10 @@
+package jaws.presentation.views;
+
+import javax.swing.JButton;
+
+public interface PresetDelegate {
+
+	public void loadPresetClicked();
+	public void savePresetClicked();
+	public void newPresetClicked();
+}

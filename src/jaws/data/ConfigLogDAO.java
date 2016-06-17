5fb17147bedd1217b126dca483497009400de293
Roy Geesing <roy.geesing@hispeed.ch>
2016-06-17 16:21:40 +0200

    prepared ConfigLogDao for possible reconnection

--- src/jaws/data/ConfigLogDAO.java
+++ src/jaws/data/ConfigLogDAO.java
@@ -19,7 +19,8 @@ import org.json.JSONObject;
 
 public class ConfigLogDAO implements ConfigDAO, Runnable {
 
-	private final Socket server;
+	private final String server;
+	private final int port;
 
 	private final int interval;
 	private final Consumer<List<JSONObject>> logCallback;
@@ -29,8 +30,9 @@ public class ConfigLogDAO implements ConfigDAO, Runnable {
 
 	private Map<String, JSONObject> configsToSave = new ConcurrentHashMap<>();
 
-	public ConfigLogDAO(Socket server, int interval, Consumer<List<JSONObject>> logCallback, Consumer<Map<String, JSONObject>> configCallback) {
+	public ConfigLogDAO(String server, int port, int interval, Consumer<List<JSONObject>> logCallback, Consumer<Map<String, JSONObject>> configCallback) {
 		this.server = server;
+		this.port = port;
 		this.interval = interval;
 		this.logCallback = logCallback;
 		this.configCallback = configCallback;
@@ -56,86 +58,90 @@ public class ConfigLogDAO implements ConfigDAO, Runnable {
 		long lastLogUpdate = -1;
 
 		while(true) {
-			long lastStartingTime = System.currentTimeMillis();
-
-			JSONObject requestJson = new JSONObject();
-
-			JSONObject updateLogsJson = new JSONObject();
-			requestJson.put("updateLogs", updateLogsJson);
-
-			if(lastLogUpdate > 0) {
-				updateLogsJson.put("lastUpdate", lastLogUpdate);
-			}
-
-			synchronized (updateConfigs) {
-				// TODO stuff with updateConfigs
-				if(updateConfigs) {
-					JSONObject updateConfigsJson = new JSONObject();
-					requestJson.put("updateConfigs", updateConfigsJson);
-
-					updateConfigs = false;
-				}
-			}
-			synchronized (configsToSave) {
-				// TODO Do stuff with configs
-				if(configsToSave.size() > 0) {
-					JSONObject configsToSaveJson = new JSONObject();
-					requestJson.put("saveConfigs", configsToSaveJson);
-					
-					for(Entry<String, JSONObject> configToSaveJson : configsToSave.entrySet()) {
-						configsToSaveJson.put(configToSaveJson.getKey(), configToSaveJson.getValue());
-					}
-					
-					configsToSave.clear();
-				}
-			}
-			// TODO write to and read from socket
+			Socket connection;
 			try {
-				OutputStream out = server.getOutputStream();
-				out.write(requestJson.toString().getBytes());
-				
-				BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
-				StringBuilder stringBuilder = new StringBuilder();
-				{
-					String line;
-					while((line = in.readLine()) != null) {
-						stringBuilder.append(line);
-					}
-				}
-				JSONObject response = new JSONObject(stringBuilder.toString());
-				
-				if(response.has("logUpdate")) {
-					JSONObject logUpdate = response.getJSONObject("logUpdate");
-					lastLogUpdate = logUpdate.getLong("lastUpdate");
-					JSONArray logsJson = logUpdate.getJSONArray("logs");
-					List<JSONObject> logs = new ArrayList<>();
-					for(int i=0; i<logsJson.length(); i++) {
-						logs.add(logsJson.getJSONObject(i));
-					}
-					
-					logCallback.accept(logs);
-				}
-				
-				if(response.has("configUpdate")) {
-					JSONObject configUpdate = response.getJSONObject("configUpdate");
-					Map<String, JSONObject> newConfigs = new HashMap<>();
-					Iterator<String> keys = configUpdate.keys();
-					while(keys.hasNext()) {
-						String key = keys.next();
-						JSONObject log = configUpdate.getJSONObject(key);
-						newConfigs.put(key, log);
-					}
-					
-					configCallback.accept(newConfigs);
-				}
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
+				connection = new Socket(server, port);
 			
-			try {
-				Thread.sleep(Math.max(interval - (System.currentTimeMillis() - lastStartingTime), 0));
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+				long lastStartingTime = System.currentTimeMillis();
+	
+				JSONObject requestJson = new JSONObject();
+	
+				JSONObject updateLogsJson = new JSONObject();
+				requestJson.put("updateLogs", updateLogsJson);
+	
+				if(lastLogUpdate > 0) {
+					updateLogsJson.put("lastUpdate", lastLogUpdate);
+				}
+	
+				synchronized (updateConfigs) {
+					if(updateConfigs) {
+						JSONObject updateConfigsJson = new JSONObject();
+						requestJson.put("updateConfigs", updateConfigsJson);
+	
+						updateConfigs = false;
+					}
+				}
+				synchronized (configsToSave) {
+					if(configsToSave.size() > 0) {
+						JSONObject configsToSaveJson = new JSONObject();
+						requestJson.put("saveConfigs", configsToSaveJson);
+						
+						for(Entry<String, JSONObject> configToSaveJson : configsToSave.entrySet()) {
+							configsToSaveJson.put(configToSaveJson.getKey(), configToSaveJson.getValue());
+						}
+						
+						configsToSave.clear();
+					}
+				}
+				try {
+					OutputStream out = connection.getOutputStream();
+					out.write(requestJson.toString().getBytes());
+					
+					BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+					StringBuilder stringBuilder = new StringBuilder();
+					{
+						String line;
+						while((line = in.readLine()) != null) {
+							stringBuilder.append(line);
+						}
+					}
+					JSONObject response = new JSONObject(stringBuilder.toString());
+					
+					if(response.has("logUpdate")) {
+						JSONObject logUpdate = response.getJSONObject("logUpdate");
+						lastLogUpdate = logUpdate.getLong("lastUpdate");
+						JSONArray logsJson = logUpdate.getJSONArray("logs");
+						List<JSONObject> logs = new ArrayList<>();
+						for(int i=0; i<logsJson.length(); i++) {
+							logs.add(logsJson.getJSONObject(i));
+						}
+						
+						logCallback.accept(logs);
+					}
+					
+					if(response.has("configUpdate")) {
+						JSONObject configUpdate = response.getJSONObject("configUpdate");
+						Map<String, JSONObject> newConfigs = new HashMap<>();
+						Iterator<String> keys = configUpdate.keys();
+						while(keys.hasNext()) {
+							String key = keys.next();
+							JSONObject log = configUpdate.getJSONObject(key);
+							newConfigs.put(key, log);
+						}
+						
+						configCallback.accept(newConfigs);
+					}
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+				
+				try {
+					Thread.sleep(Math.max(interval - (System.currentTimeMillis() - lastStartingTime), 0));
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
 			}
 		}
 	}

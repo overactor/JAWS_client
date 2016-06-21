58603b0e2c3c92b29233c13f1cb74c20ad0fcd5d
overactor <rik.degraaff@gmx.ch>
2016-06-21 23:17:17 +0200

    Added some more JavaDoc

--- src/jaws/data/preset/PresetDAOFactory.java
+++ src/jaws/data/preset/PresetDAOFactory.java
@@ -4,8 +4,20 @@ import java.io.File;
 import java.util.Optional;
 import java.util.function.Supplier;
 
+/**
+ * A class to manage {@link jaws.data.preset.PresetDAO PresteDAO objects}
+ * 
+ * @author Rik
+ */
 public class PresetDAOFactory {
 	
+	/**
+	 * Creates a PresetDAO with a give save- and openFilePicker
+	 * 
+	 * @param saveFilePicker is called when a Preset is exported
+	 * @param openFilePicker is called when a Preset is imported
+	 * @return
+	 */
 	public static PresetDAO createPresetDAO(Supplier<Optional<File>> saveFilePicker, Supplier<Optional<File>> openFilePicker) {
 		return new FilePresetDAO("presets.json", saveFilePicker, openFilePicker);
 	}
